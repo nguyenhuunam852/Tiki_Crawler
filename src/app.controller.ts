1d2961dc3b2d88b6cc56b3634ceb4eae51987285
Nguyễn Hữu Nam <evannguyen12399@gmail.com>
2022-07-01 13:30:46 +0700

    New

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,23 +5,6 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
-  simpleStringify(object) {
-    var simpleObject = {};
-    for (var prop in object) {
-      if (!object.hasOwnProperty(prop)) {
-        continue;
-      }
-      if (typeof (object[prop]) == 'object') {
-        continue;
-      }
-      if (typeof (object[prop]) == 'function') {
-        continue;
-      }
-      simpleObject[prop] = object[prop];
-    }
-    return JSON.stringify(simpleObject); // returns cleaned up JSON
-  };
-
   @Get()
   async getNewestBook() {
     let result = await this.appService.getListBooks();
